63466f33d5337ea091e8924a88038bced6f7928a
ZingaG <trad_bar@mail.ru>
2023-03-09 14:50:06 +0300

    CreateArray/PrintArray functions added, Task 3 completed

--- Program.cs
+++ Program.cs
@@ -30,9 +30,56 @@ int AinB(int a, uint b)
     return result;
 }
 
+int NumberSum(int a)
+{
+    int result = 0;
+
+    while(a > 0)
+    {
+        result += a % 10;
+        a = a/10;
+    }
+
+    return result;
+    
+}
+
+int[] CreateArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = TakeDigit($"Input array[{i}] = ");
+    }
+
+    return array;
+
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("Your array = [{0}]", string.Join(", ", array));
+}
+
 // Task 1
 
+/*
 int a = TakeDigit("Input A (digit) = ");
 uint b = TakePositiveDigit("Input B (degree) = ");
 
 System.Console.WriteLine("Result = " + AinB(a,b));
+*/
+
+// Task 2
+
+/*
+int a = TakeDigit("Input A = ");
+System.Console.WriteLine("Sum = " + NumberSum(a));
+*/
+
+// Task 3
+
+/*
+int[] Array = CreateArray();
+PrintArray(Array);
+*/
